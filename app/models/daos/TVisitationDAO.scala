49edb7d08b43003ac720f379ec3f92e0c3c0d6a3
Moverr <moverr@gmail.com>
2023-01-01 19:07:27 +0300

    Visitation DAO

---
+++ app/models/daos/TVisitationDAO.scala
@@ -0,0 +1,23 @@
+package models.daos
+
+import models.entities.{Visitation, VisitationTable}
+import slick.lifted.TableQuery
+
+import scala.concurrent.Future
+
+trait TVisitationDAO {
+
+  val visitationTable: TableQuery[VisitationTable]
+
+  //todo: create
+  def add(visitation: Visitation): Future[Visitation]
+
+  //todo: list
+  def list(offset: Long, limit: Long): Future[Seq[Visitation]]
+
+  //todo: get by id
+  def get(id: Long): Future[Option[Visitation]]
+
+  //todo: update
+  def update(id: Long, visitation: Visitation): Future[Either[Throwable, String]]
+}
--- app/models/daos/VisitationDAO.scala
+++ app/models/daos/VisitationDAO.scala
@@ -12,34 +12,35 @@ import slick.lifted.TableQuery
 
 
 
+@Singleton
 class VisitationDAO  @Inject()
-(protected  val dbConfigProvider: DatabaseConfigProvider)(  implicit executionContext: ExecutionContext){
+(protected  val dbConfigProvider: DatabaseConfigProvider)(  implicit executionContext: ExecutionContext) extends TVisitationDAO {
 
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
-  lazy val visitationTable = TableQuery[VisitationTable]
+  override lazy val visitationTable = TableQuery[VisitationTable]
 
   import dbConfig._
 
   //todo: create
-  def add(visitation:Visitation): Future[Visitation] = {
+  override def add(visitation:Visitation): Future[Visitation] = {
     val query = visitationTable.returning(visitationTable) += visitation
     db.run(query)
   }
   //todo: list
-  def list(offset:Long,limit:Long):Future[Seq[Visitation]] ={
+  override def list(offset:Long,limit:Long):Future[Seq[Visitation]] ={
     db.run(visitationTable.sortBy(_.created_at).take(limit).drop(offset).result)
   }
   //todo: get by id
-  def get(id:Long):Future[Option[Visitation]]={
+  override def get(id:Long):Future[Option[Visitation]]={
     db.run(visitationTable.filter(_.id === id).result.headOption)
   }
   //todo: update
-  def update(id:Long,visitation: Visitation): Future[Either[Throwable,String]] = {
+  override def update(id:Long,visitation: Visitation): Future[Either[Throwable,String]] = {
 
     val query =visitationTable.filter(_.id === id).update(visitationTable) = visitation
     db.run(query)
     ???
 
   }
-// ??
+  // ??
 }
