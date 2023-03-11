6e8c03bbd87044015899839805a784f1cf85d343
Moverr <moverr@gmail.com>
2023-03-12 00:55:08 +0300

    Debuggging

--- app/controllers/requests/VisitationRequest.scala
+++ app/controllers/requests/VisitationRequest.scala
@@ -1,6 +1,4 @@
 package controllers.requests
 
-import akka.http.javadsl.model.DateTime
-
 case class VisitationRequest(hostId:Long, guestId:Long,  timeIn:String, timeOut:String, status:String, timezone:String)
 
