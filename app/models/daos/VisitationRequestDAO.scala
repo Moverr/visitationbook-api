cbf5fe08363f9c752ce301f1d937b163ff16c4a2
Moverr <moverr@gmail.com>
2023-06-28 22:58:14 +0300

    Debuggging

--- app/models/daos/VisitationRequestDAO.scala
+++ app/models/daos/VisitationRequestDAO.scala
@@ -4,28 +4,27 @@ import controllers.requests.RequestVisitation
 import models.entities.{VisitationRequestTable, visitationRequestEntity}
 import play.api.db.slick.DatabaseConfigProvider
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import slick.jdbc.JdbcProfile
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.TableQuery
 
 import scala.concurrent.{ExecutionContext, Future}
-
-
 import scala.concurrent.impl.Promise
 
 
+@Singleton
 class VisitationRequestDAO    @Inject()(private val dbConfigProvider: DatabaseConfigProvider)   (implicit executionContext: ExecutionContext)
 {
 
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
-  lazy val visitationTable = TableQuery[VisitationRequestTable]
+  lazy val visitationRequestTable = TableQuery[VisitationRequestTable]
 
   import dbConfig._
 
   //todo: create
    def create(visitation: visitationRequestEntity): Future[visitationRequestEntity] = {
-    val query = visitationTable.returning(visitationTable) += visitation
+    val query = visitationRequestTable.returning(visitationRequestTable) += visitation
     db.run(query)
   }
 
@@ -34,24 +33,24 @@ class VisitationRequestDAO    @Inject()(private val dbConfigProvider: DatabaseCo
     //    val query = for{
     //      (visitation,host,guest)<-
     //    }
-    db.run(visitationTable.sortBy(_.dateCreated).take(offset).drop(limit).result)
+    db.run(visitationRequestTable.sortBy(_.dateCreated).take(offset).drop(limit).result)
   }
 
   //todo: get by id
-    def get(id: Long): Future[Option[visitationRequestEntity]] = {
-    db.run(visitationTable.filter(_.id === id).result.headOption)
+  def get(id: Long): Future[Option[visitationRequestEntity]] = {
+    db.run(visitationRequestTable.filter(_.id === id).result.headOption)
   }
 
   //todo: update
     def update(id: Long, visitation: visitationRequestEntity): Future[visitationRequestEntity] = {
 
-    val query = visitationTable.filter(_.id === id).update(visitation)
+    val query = visitationRequestTable.filter(_.id === id).update(visitation)
     db.run(query)
     Future.successful(visitation)
   }
 
     def delete(id: Long): Any = {
-    val query = visitationTable.filter(_.id === id).delete
+    val query = visitationRequestTable.filter(_.id === id).delete
     db.run(query)
   }
 
