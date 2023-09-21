eb3e46f16d0928910143e53c6aad557bbb99fdf6
Muyinda Rogers <moverr@gmail.com>
2023-09-21 20:19:10 +0300

    :Debuggging :

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,32 +1,32 @@
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME datecreated TO date_created;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE visitationrequests
     RENAME invtype TO inv_type;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME departmentid TO department_id;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME officeid TO office_id;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME guestid TO guest_id;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME hostid TO host_id;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE visitationrequests
     RENAME createdby TO created_by;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME dateupdated TO date_updated;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     RENAME updatedby TO updated_by;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     ADD COLUMN start_date timestamp without time zone;
 
-ALTER TABLE "default".visitationrequests
+ALTER TABLE  visitationrequests
     ADD COLUMN end_date timestamp without time zone;
