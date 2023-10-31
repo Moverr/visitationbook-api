c146ac1b0ff078d4f8a6ac453dca9ac595bd8c8b
Muyinda Rogers <moverr@gmail.com>
2023-10-31 18:25:51 +0300

    :Debugging :

--- scripts/PackageApplication.bat
+++ scripts/PackageApplication.bat
@@ -1,6 +1,14 @@
 @echo off
 set app="Visitation Book API"
 echo  %app%
+
+set SBT_PATH="C:\\Program Files (x86)\\sbt\\bin\\sbt.bat"
+
+if not exist %SBT_PATH% (
+    echo sbt not found at %SBT_PATH%
+    exit /b
+)
+
 cd ../
 call sbt dist
 
