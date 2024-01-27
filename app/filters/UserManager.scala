17c74421e04edc22c5367b1acdec73182ac936c1
Muyinda Rogers <moverr@gmail.com>
2024-01-27 19:28:59 +0300

    :DEb

--- app/filters/UserManager.scala
+++ app/filters/UserManager.scala
@@ -4,6 +4,9 @@ import play.api.mvc.RequestHeader
 
 case class UserManager() {
   def isAuthenticated(requestHeader: RequestHeader): Boolean = {
+
+    println(requestHeader)
+    println(" reached the Authentication mechanism ..")
     true
   }
 
