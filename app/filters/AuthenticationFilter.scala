87ec325ad7ad4adc6d9f08838461a4fcc669ee43
Muyinda Rogers <moverr@gmail.com>
2024-02-04 15:17:25 +0300

    Debuggging ...

--- app/filters/AuthenticationFilter.scala
+++ app/filters/AuthenticationFilter.scala
@@ -12,11 +12,31 @@ class AuthenticationFilter @Inject()(userManager: UserManager, implicit val mat:
 {
  @Override def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
 
-   println(requestHeader.headers)
+   println("Testing out some info")
 
    val dataMap: Map[String, Seq[String]] = requestHeader.headers.toMap
-   //{ case (k, v) => k -> Json.fromString(v.toString) }.toMap
+   val bearerInfo:Option[Seq[String]] = dataMap.get("Authorization")
 
+   println("Dealing in the nature and also .....")
+
+   val token:Option[String] = for {
+     values <- bearerInfo
+     value <- values.headOption
+
+   }yield {
+     println("Indeed interesting")
+     println(value)
+     value
+   }
+
+   val res:String = for {
+     result <- token.get
+   }yield result
+
+   println("Result nature ......")
+
+   //{ case (k, v) => k -> Json.fromString(v.toString) }.toMap
+   println(res)
    println(dataMap)
 
     if (userManager.isAuthenticated(requestHeader)) {
--- app/filters/UserManager.scala
+++ app/filters/UserManager.scala
@@ -1,15 +1,41 @@
 package filters
+import play.libs.ws._
+import scala.concurrent.ExecutionContext
+
+
 
 import play.api.mvc.RequestHeader
 import services.ClientRequestHeader
+import cats.implicits._
 
-case class UserManager() {
+import play.api.libs.ws.{WSClient, WSResponse}
+
+import javax.inject.Inject
+import scala.concurrent.Future
+ class UserManager @Inject() (   ws: WSClient)(implicit ec: ExecutionContext) {
+  protected val authorization: String = "Authorization"
+
   def isAuthenticated(requestHeader: RequestHeader): Boolean = {
 
-    val authHeader = requestHeader.headers;
+    val authHeader = requestHeader.headers.toMap
+   // val clientRequestHeaders = ClientRequestHeader(authHeader)
+    println(" reached the Authentication mechanism ..")
+   /* //todo: get authentication
+   val authenticationHeader:String=  clientRequestHeaders.getHeaderString(authorization).getOrElse("EMPTY")
+    //todo: validate token :
+    val basicUrl = "http://52.207.255.31:8082";
+    val fullUrl = s"$basicUrl/v1/auth"
+    val username = "superadmin"
+    val password = "password"
+    println(" reached the Authentication mechanism ..")
+    val apiResponse: Future[WSResponse] = ws.url(fullUrl).get()
+
     println(requestHeader.headers)
     println(" reached the Authentication mechanism ..")
+    */
     true
   }
+
+
 
 }
