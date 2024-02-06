d1f7a46511d03e34f73ea00447be0b1ad405790b
Muyinda Rogers <moverr@gmail.com>
2024-02-06 18:10:20 +0300

    Debugging..

--- app/filters/AuthenticationFilter.scala
+++ app/filters/AuthenticationFilter.scala
@@ -1,43 +1,33 @@
 package filters
 
 import akka.stream.Materializer
+import annotation.CLogger
 import play.api.mvc.Results.Unauthorized
+import play.api.mvc._
 
 import javax.inject.Inject
-import play.api.mvc._
-import play.libs.Json
-
 import scala.concurrent.Future
-class AuthenticationFilter @Inject()(userManager: UserManager, implicit val mat: Materializer) extends Filter
-{
- @Override def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
 
-   println("Testing out some info")
+@CLogger
+class AuthenticationFilter @Inject()(userManager: UserManager, implicit val mat: Materializer) extends Filter {
+  @Override def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
 
-   val dataMap: Map[String, Seq[String]] = requestHeader.headers.toMap
-   val bearerInfo:Option[Seq[String]] = dataMap.get("Authorization")
+    println("Testing out some info")
 
-   println("Dealing in the nature and also .....")
-
- val token:Option[String]  = bearerInfo flatMap(x=>x.headOption.filter(_.nonEmpty))
- val res: String = token.getOrElse("NONE")
-
-//   val res:String = for {
-//     result <- token.get
-//   }yield result
-
-   println("Result nature ......")
-
-   //{ case (k, v) => k -> Json.fromString(v.toString) }.toMap
-   println(res)
-   println(dataMap)
+    val dataMap: Map[String, Seq[String]] = requestHeader.headers.toMap
+    val bearerInfo: Option[Seq[String]] = dataMap.get("Authorization")
 
 
 
-    if (userManager.isAuthenticated(requestHeader)) {
+    val token: Option[String] = bearerInfo flatMap (x => x.headOption.filter(_.nonEmpty))
+    val res: String = token.getOrElse("NONE")
+    println(s" token result $res ")
+
+    if (!res.equalsIgnoreCase("none") && userManager.isAuthenticated(res)) {
       nextFilter(requestHeader)
     } else {
       Future.successful(Unauthorized("Unauthorized"))
     }
+
   }
 }
--- app/filters/UserManager.scala
+++ app/filters/UserManager.scala
@@ -10,67 +10,59 @@ import play.api.libs.ws.ahc.AhcWSClient
 import play.api.mvc.RequestHeader
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 class UserManager {
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
-  def isAuthenticated(requestHeader: RequestHeader): Boolean = {
+  def isAuthenticated(bearerToken: String): Future[Boolean]   = {
 
-    val authorization: String = "Authorization"
+    val bearer = bearerToken.split(" ") match {
+      case Array(_, token) => s"Bearer $token"
+      case _ => throw new IllegalArgumentException("Invalid bearer token format")
+    }
+
+    println(s" token result $bearer ")
     val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()(system)
 
-    val client: APIClient =  new  APIClient(ws = AhcWSClient.apply() )
+    val client: APIClient = new APIClient(ws = AhcWSClient.apply())
 
-
-    val authHeader = requestHeader.headers.toMap
-    // val clientRequestHeaders = ClientRequestHeader(authHeader)
-    println(" reached the Authentication mechanism ..")
-    //todo: get authentication
-    // val authenticationHeader:String=  clientRequestHeaders.getHeaderString(authorization).getOrElse("EMPTY")
-    //todo: validate token :
     val basicUrl = "http://52.207.255.31:8082";
-    val fullUrl = s"$basicUrl/v1/auth"
-    val username = "superadmin"
-    val password = "password"
-    println(" reached the Authentication mechanism ..")
-
-    val jsObject: JsObject = Json.obj {
-      "username" -> username
-      "password" -> password
-    }
-
-      val myMap: Map[String, String] = Map(
-        "username" -> username,
-        "password" -> password
-      )
-
-      val jsonObject: JsObject = Json.toJson(myMap).as[JsObject]
-
-      println(jsonObject)
+    val fullUrl = s"$basicUrl/v1/auth/validate"
 
 
+    val jsonObject: JsObject = Json.toJson(Map[String, String]()).as[JsObject]
 
-    println("Full jsObject ")
-    println(jsObject)
-    val headers: Map[String, String] = Map()+("Content-Type"->"application/json")
+    val headers: Map[String, String] = Map("Authorization" -> bearer)
+
+    println(s" Header  result $headers ")
     val apiResponse: Future[WSResponse] = client.postRequest(fullUrl, headers, jsonObject)
 
+    apiResponse.onComplete {
+      case Failure(exception) =>
+        println(s"An error occured : ${exception.getMessage}")
+
+      case Success(value) =>
+        println(s" ... Status :: -----  : ${value.status}")
+        println(s" ... body :: -----  : ${value.body}")
+    }
 
     apiResponse.map { response =>
       val status: Int = response.status
       println(s" ... Status :: -----  : ${status}")
       val body: String = response.body
       println(s" ... Body :: -----  : ${body}")
+      true
     }.recover {
       case ex: Throwable => println(s"An error occured : ${ex.getMessage}")
+        false
     }
 
 
-    println(requestHeader.headers)
-    println(" reached the Authentication mechanism ..")
+    //println(" reached the Authentication mechanism ..")
 
-    true
+
   }
 
 
